79e469c498353c9f46b9e8153db5e3441883f4cc
John Castillo <john.gnu@gmail.com>
2024-05-06 06:06:20 -0400

    Updates

---
+++ p4/WEbPHP/menu.php
@@ -0,0 +1,8 @@
+    <header>
+        <h1>WEB PHP</h1>
+    </header>  
+    <nav>
+        <a href="">Inicio</a>
+        <a href="">Personas</a>
+        <a href="">Cuentas</a>
+    </nav>
---
+++ p4/WebJava/src/java/com/john/Servlet.java
@@ -0,0 +1,53 @@
+package com.john;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author John Castillo Valencia
+ */
+@WebServlet(name = "Servlet", urlPatterns = {"/Servlet"})
+public class Servlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Servlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Servlet at " + request.getContextPath() + "</h1>");
+            out.println("Dato 1: " + request.getParameter("dato1") + "<br>");
+            out.println("Dato 2: " + request.getParameter("dato2") + "<br>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+}
